c47e27affeb02c6599b8125ee10e1b234a4efae2
Divya Tagtachian <hello@shortdiv.com>
2020-06-08 20:57:48 -0500

    updates

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,58 @@
-import Vue from "vue";
 import Vuex from "vuex";
+import Vue from "vue";
 
 Vue.use(Vuex);
 
+// fake API call //
+let inventory = {
+  chips: {
+    stock: 40
+  }
+};
+
+var pingInventory = function(item) {
+  return new Promise(resolve => {
+    setTimeout(function() {
+      resolve(inventory[item]);
+    }, 3000);
+  });
+};
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    supply: 40,
+    isRestocking: false,
+    isDispensing: false
+  },
+  actions: {
+    fetchFromInventory(context) {
+      context.commit("isRestocking", true);
+      pingInventory("chips")
+        .then(inventory => {
+          context.commit("stockItems", inventory.stock);
+        })
+        .finally(() => context.commit("isRestocking", false));
+    },
+    dispense(context) {
+      context.commit("isDispensing", true);
+      setTimeout(() => {
+        context.commit("dispense");
+        context.commit("isDispensing", false);
+      }, 3000);
+    }
+  },
+  getters: {},
+  mutations: {
+    isRestocking(state, payload) {
+      state.isRestocking = payload;
+    },
+    isDispensing(state, payload) {
+      state.isDispensing = payload;
+    },
+    dispense(state) {
+      state.supply--;
+    },
+    stockItems(state) {
+      state.supply = 40;
+    }
+  }
 });
